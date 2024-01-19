4c3b1e0b006a40bc50cbe7a65d0de7afe2019c14
frankdin <frankdin@yahoo.com>
2024-01-19 11:46:06 -0500

    Added code to make the grid sqaures wrap around to the next row

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 "use strict";
 
 const main = document.querySelector("#main");
+main.style.flexWrap = 'wrap';
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 10; j++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.style.border = "black 1px solid";
+        gridSquare.style.width = '9px';
+        gridSquare.style.height = '9px';
 
-for (let i = 0; i < 10; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.style.border = "black 1px solid";
-    gridSquare.style.width = '10px';
-    gridSquare.style.height = '10px';
-    main.appendChild(gridSquare);
+        main.appendChild(gridSquare);
+    }
+
 }
+
 
 
 
