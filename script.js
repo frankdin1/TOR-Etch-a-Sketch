98e9dbd7cb8f8fcc54741109ab98b1749d6cf4e5
frankdin <frankdin@yahoo.com>
2024-01-19 16:40:16 -0500

    Added code to addGridSquares function so that grid changes as slider moves left or right.

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ main.style.flexWrap = 'wrap';
 
 function addGridSquares(gridDims) {
     const mainDimensionInInt = parseFloat(main.clientHeight - main.offsetTop)
+
+    //If there are any childNodes, remove them to make way for the new childnodes that will show up every time the eventlistener is run.
+    while (main.firstChild) {
+        main.removeChild(main.lastChild);
+    }
     let gridSquareBorderWidthInInt;
 
     for (let i = 0; i < gridDims; i++) {
+        console.log("Slider value in addGridSquares method: ", gridDims)
         for (let j = 0; j < gridDims; j++) {
             gridSquare = document.createElement('div');
+            console.log(gridSquare.style.display)
             gridSquare.style.border = "blue 2px solid";
             gridSquareBorderWidthInInt = parseFloat(gridSquare.style.borderWidth)
             gridSquare.style.width = (mainDimensionInInt / gridDims) - 2 * gridSquareBorderWidthInInt.toString() + 'px';
@@ -28,23 +35,8 @@ function addGridSquares(gridDims) {
 
 addGridSquares(slider.value);
 
-// const gridSquareWidthInInt = parseFloat(gridSquare.style.width);
-// const mainDimensionInInt = parseFloat(main.clientHeight - main.offsetTop)
-// const gridSquareBorderWidthInInt = parseFloat(gridSquare.style.borderWidth)
-// console.log((mainDimensionInInt / slider.value) - 2 * gridSquareBorderWidthInInt)
-// console.log(gridSquareWidthInInt);
-// console.log();
-// gridSquareWidthInInt) - 2 * parseInt(gridSquare.style.borderWidth)
-// slider.addEventListener('input', function () {
-//     gridHeight.innerHTML = slider.value;
-//     gridWidth.innerHTML = slider.value;
-//     addGridSquares(gridHeight.innerHTML);
-// })
-
-
-//To get up to 100 squares on each side, how many pixels would I need?
-//1 grid square = 1px border + length
-//Main div dimension: 900 X 900 px
-//Individual pixel size = (main div dimension / number of pixels) - 2(grid square border radius)
-
-
+slider.addEventListener('input', function () {
+    gridHeight.innerHTML = slider.value;
+    gridWidth.innerHTML = slider.value;
+    addGridSquares(slider.value);
+})
