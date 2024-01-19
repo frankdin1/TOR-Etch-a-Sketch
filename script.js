0810acedb7dec04804441e0d227bbbf277f98a56
frankdin <frankdin@yahoo.com>
2024-01-19 13:06:14 -0500

    Made a function out of piece of code that add grid squares to the screen.

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 "use strict";
 
 const main = document.querySelector("#main");
+let gridSquare;
 main.style.flexWrap = 'wrap';
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.style.border = "blue 1px solid";
-        gridSquare.style.width = '54px';
-        gridSquare.style.height = '54px';
+// for (let i = 0; i < 16; i++) {
+//     for (let j = 0; j < 16; j++) {
+//         gridSquare = document.createElement('div');;
+//         gridSquare.style.border = "blue 2px solid";
+//         gridSquare.style.width = '52.25px';
+//         gridSquare.style.height = gridSquare.style.width;
+//         main.appendChild(gridSquare);
+//     }
 
-        main.appendChild(gridSquare);
+// }
+
+
+function addGridSquares() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            gridSquare = document.createElement('div');;
+            gridSquare.style.border = "blue 2px solid";
+            gridSquare.style.width = '52.25px';
+            gridSquare.style.height = gridSquare.style.width;
+            main.appendChild(gridSquare);
+        }
+
     }
-
 }
 
-
-//For a main box of width 110px and height 110px:
-//grid square borders: 1px
-//10 X 10 grid: 9px X 9px each
-//11 X 11 grid: 8px X 8px each
-//For each extra grid square, reduce the length by 1px?
+addGridSquares();
 
 //To get up to 100 squares on each side, how many pixels would I need?
 //1 grid square = 1px border + length
-//With a div that has length = 1000px
+//Main div dimension: 900 X 900 px
+//Individual pixel size = (main div dimension / number of pixels) - 2(grid square border radius)
 
 
