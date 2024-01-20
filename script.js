cf40ee535fc98bf239f98d0f5f46391ec8705ecc
frankdin <frankdin@yahoo.com>
2024-01-19 20:31:39 -0500

    Added code to give grid squares divs individual ids.

--- script.js
+++ script.js
@@ -5,27 +5,30 @@ const slider = document.querySelector("input");
 const gridSize = document.querySelector("#grid-size");
 const gridHeight = document.querySelector('#grid-height')
 const gridWidth = document.querySelector('#grid-width')
-let gridSquare;
+let gridSquare = document.createElement('div')
+gridSquare.setAttribute('id', 'grid-square');
+main.appendChild(gridSquare);
+const mainChildNodes = document.querySelectorAll("grid-square")
+
 gridHeight.innerHTML = slider.value;
 gridWidth.innerHTML = slider.value;
 main.style.flexWrap = 'wrap';
 
 function addGridSquares(gridDims) {
+    //In the browser, under developer tools, if you run `cosnole.dir(main), you will see the values of clientHeight and offsetTop
     const mainDimensionInInt = parseFloat(main.clientHeight - main.offsetTop)
 
     //If there are any childNodes, remove them to make way for the new childnodes that will show up every time the eventlistener is run.
     while (main.firstChild) {
         main.removeChild(main.lastChild);
     }
-    let gridSquareBorderWidthInInt;
 
     for (let i = 0; i < gridDims; i++) {
-        console.log("Slider value in addGridSquares method: ", gridDims)
         for (let j = 0; j < gridDims; j++) {
             gridSquare = document.createElement('div');
-            console.log(gridSquare.style.display)
+            gridSquare.setAttribute('id', 'grid-square');
             gridSquare.style.border = "blue 2px solid";
-            gridSquareBorderWidthInInt = parseFloat(gridSquare.style.borderWidth)
+            const gridSquareBorderWidthInInt = parseFloat(gridSquare.style.borderWidth)
             gridSquare.style.width = (mainDimensionInInt / gridDims) - 2 * gridSquareBorderWidthInInt.toString() + 'px';
             gridSquare.style.height = gridSquare.style.width;
             main.appendChild(gridSquare);
@@ -33,10 +36,19 @@ function addGridSquares(gridDims) {
     }
 }
 
+//This will set the initial value of the grid when open the page.
 addGridSquares(slider.value);
 
+//Every time the slider moves, its value will be displayed on screen, and the addGridSquares function will run.
 slider.addEventListener('input', function () {
     gridHeight.innerHTML = slider.value;
     gridWidth.innerHTML = slider.value;
     addGridSquares(slider.value);
 })
+
+// for (let i = 0; i < mainChildNodes.length; i++) {
+//     mainChildNodes[i].addEventListener('mouseover', function () {
+//         console.log('I am hovering');
+//         mainChildNodes[i].style.backgroundColor = 'black';
+//     })
+// }
